f1dcd68416efbabebbbe1ea0f13ffc8e6411a097
root <root@TylerPC.localdomain>
2023-09-04 22:07:22 -0700

    Initial commit

---
+++ src/main/java/net/cooldudetyler/minecraftbeyond/MinecraftBeyondClient.java
@@ -0,0 +1,10 @@
+package net.cooldudetyler.minecraftbeyond;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftBeyondClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/cooldudetyler/minecraftbeyond/MinecraftBeyondGenerator.java
@@ -0,0 +1,11 @@
+package net.cooldudetyler.minecraftbeyond;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MinecraftBeyondGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
